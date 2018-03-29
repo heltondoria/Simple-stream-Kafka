4acb1a19d5b4193709410f70eac114ca5788b1e9
Helton Costa <helton.doria@gmail.com>
2018-03-29 00:26:44 -0300

    correções finais

--- consumer.py
+++ consumer.py
@@ -1,16 +1,13 @@
 from flask import Flask, Response
 from kafka import KafkaConsumer
 
-# connect to Kafka server and pass the topic we want to consume
-consumer = KafkaConsumer('my-topic', group_id='view', bootstrap_servers=['192.168.2.102:9092', '192.168.2.102:9093', '192.168.2.102:9094'])
+consumer = KafkaConsumer('my-topic', group_id='view', bootstrap_servers=['192.168.2.102:9092'])
 
-# Continuously listen to the connection and print messages as recieved
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    # return a multipart response
     return Response(kafkastream(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
--- producer.py
+++ producer.py
@@ -1,51 +1,38 @@
-# producer.py
 import time
 import cv2
 from kafka import KafkaProducer
-#  connect to Kafka
 from kafka.errors import KafkaError
 import logging
 
 producer = KafkaProducer(bootstrap_servers=['192.168.2.102:9092'], retries=5)
-# Assign a topic
 topic = 'my-topic'
 
 
 def video_emitter(video):
-    # Open the video
     video = cv2.VideoCapture(video)
     print(' emitting.....')
 
-    # read the file
     while video.isOpened:
-        # read the image in each frame
         success, image = video.read()
 
-        # check if the file has read the end
         if not success:
             break
 
-        # convert the image png
         ret, jpeg = cv2.imencode('.png', image)
-        # Convert the image to bytes and send to kafka
-        # Asynchronous by default
         future = producer.send(topic, jpeg.tobytes())
 
-        # Block for 'synchronous' sends
         try:
             record_metadata = future.get(timeout=10)
         except KafkaError:
-            # Decide what to do if produce request failed...
             logging.exception(KafkaError)
             pass
 
-        # Successful result returns assigned partition and offset
         print(record_metadata.topic)
         print(record_metadata.partition)
         print(record_metadata.offset)
         # To reduce CPU usage create sleep time of 0.2sec
         time.sleep(0.2)
-    # producer.flush()
+
     # clear the capture
     video.release()
     print('done emitting')
